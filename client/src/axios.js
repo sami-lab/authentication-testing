aa56ba7aefb3efaee375f168b1ef865b6f422681
Muhammad Sami <56019715+sami-lab@users.noreply.github.com>
2021-11-09 19:26:33 +0500

    Update axios.js

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let baseURL = "/api/v1";
-if (process.env.NODE_ENV) {
+if (process.env.NODE_ENV==="development") {
   baseURL = "http://localhost:5000/api/v1";
 }
 const instance = axios.create({
