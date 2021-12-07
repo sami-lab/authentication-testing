2c3a1812dc8d0d92e4ac530285d24dac5598a425
sami-lab <s.m.sami125@gmail.com>
2021-12-07 19:49:03 +0500

    Final

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,30 +1,30 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
-const bcrypt = require('bcryptjs');
-const crypto = require('crypto');
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const crypto = require("crypto");
 
 var UserSchema = mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, 'A User must have a name'],
+      required: [true, "A User must have a name"],
       trim: true,
     },
     email: {
       type: String,
-      required: [true, 'A User must have an email'],
+      required: [true, "A User must have an email"],
       unique: true,
       lowercase: true,
-      validate: [validator.isEmail, 'Please Provide a Valid Email'],
+      validate: [validator.isEmail, "Please Provide a Valid Email"],
     },
 
     password: {
       type: String,
 
-      required: [true, 'A User must have a Password'],
+      required: [true, "A User must have a Password"],
       minlength: [
-        8,
-        'A User Password must have more or equal then 5 characters',
+        5,
+        "A User Password must have more or equal then 5 characters",
       ],
       validate: {
         validator: function (val) {
@@ -33,19 +33,19 @@ var UserSchema = mongoose.Schema(
           );
         },
         message:
-          'Password must contain at least 5 digits with one uppercase and one special letter.',
+          "Password must contain at least 5 digits with one uppercase and one special letter.",
       },
       select: false,
     },
     confirmPassword: {
       type: String,
-      required: [true, 'Please Enter Confirm Password'],
+      required: [true, "Please Enter Confirm Password"],
       validate: {
         //Only work For Create or Save
         validator: function (val) {
           return val === this.password;
         },
-        message: 'Confirm Password Did not match with Password!!!',
+        message: "Confirm Password Did not match with Password!!!",
       },
     },
     emailVerified: {
@@ -62,9 +62,9 @@ var UserSchema = mongoose.Schema(
     toObject: { virtuals: true },
   }
 );
-UserSchema.pre('save', async function (next) {
+UserSchema.pre("save", async function (next) {
   //hashing password
-  if (!this.isModified('password')) return next();
+  if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 12);
   this.confirmPassword = undefined;
   next();
@@ -92,14 +92,14 @@ UserSchema.methods.correctPassword = async function (
 };
 
 UserSchema.methods.createEmailVerificationToken = function () {
-  const Token = crypto.randomBytes(32).toString('hex');
+  const Token = crypto.randomBytes(32).toString("hex");
   this.emailVerificationToken = crypto
-    .createHash('Sha256')
+    .createHash("Sha256")
     .update(Token)
-    .digest('hex');
+    .digest("hex");
   this.emailVerificationExpires = Date.now() + 2 * 60 * 60 * 1000;
   return Token;
 };
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 module.exports = User;
