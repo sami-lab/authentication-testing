39982c55678e7fd283efa905b73e8ca6411628f2
sami-lab <s.m.sami125@gmail.com>
2021-12-13 14:54:06 +0500

    login issue

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,11 +1,11 @@
-const crypto = require('crypto');
-const jwt = require('jsonwebtoken');
-const User = require('../Models/userModel');
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const User = require("../Models/userModel");
 
-const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
-const Email = require('../utils/emails');
-const Transaction = require('../Models/transactionModel');
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+const Email = require("../utils/emails");
+const Transaction = require("../Models/transactionModel");
 
 const createLoginToken = async (user, statusCode, req, res) => {
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
@@ -17,16 +17,16 @@ const createLoginToken = async (user, statusCode, req, res) => {
     ),
     // secure: true,//only https
     httpOnly: true, //to prevent xss
-    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+    secure: req.secure || req.headers["x-forwarded-proto"] === "https",
   };
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
-  res.cookie('jwt', token, cookieOptions);
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+  res.cookie("jwt", token, cookieOptions);
   user.password = undefined; //not saving
   user.emailVerified = undefined;
   const transactions = await Transaction.find({ user: user._id });
 
   res.status(statusCode).json({
-    status: 'success',
+    status: "success",
     token,
     data: {
       user,
@@ -50,18 +50,18 @@ exports.signUp = catchAsync(async (req, res, next) => {
   await newUser.save({ validateBeforeSave: false }); //Saving only 2 Fields
 
   //Sending Email
-  const url = process.env.APP_URL + '/verifyUser/' + verifcationToken;
+  const url = process.env.APP_URL + "/verifyUser/" + verifcationToken;
   console.log(url);
   try {
     await new Email(newUser, url).sendEmailVerification();
     res.status(200).json({
-      status: 'success',
-      message: 'Token Sent to Email',
+      status: "success",
+      message: "Token Sent to Email",
     });
   } catch (err) {
     await User.findByIdAndDelete(newUser._id);
     return next(
-      new AppError('There was an error sending an email, Try Again Later', 500)
+      new AppError("There was an error sending an email, Try Again Later", 500)
     );
   }
 });
@@ -69,32 +69,32 @@ exports.signUp = catchAsync(async (req, res, next) => {
 exports.verifyEmail = catchAsync(async (req, res, next) => {
   //Comparing Token
   const hashToken = crypto
-    .createHash('sha256')
+    .createHash("sha256")
     .update(req.params.token)
-    .digest('hex');
+    .digest("hex");
   const user = await User.findOne({
     emailVerificationToken: hashToken,
     emailVerificationExpires: { $gt: Date.now() },
   });
-  if (!user) return next(new AppError('Token is Invalid or expired', 400));
+  if (!user) return next(new AppError("Token is Invalid or expired", 400));
   //Updating Field if there token verifies
   user.emailVerified = true;
   user.emailVerificationToken = undefined;
   user.emailVerificationExpires = undefined;
   await user.save({ validateBeforeSave: false });
 
-  const homepage = `${req.protocol}://${req.get('host')}/`;
+  const homepage = `${req.protocol}://${req.get("host")}/`;
   try {
     await new Email(user, homepage, homepage).sendWelcome();
     //await new Email(newUser, url).sendWelcome();
     res.status(200).json({
-      status: 'success',
-      message: 'Email Veification Sucessful! Login To continue',
+      status: "success",
+      message: "Email Veification Sucessful! Login To continue",
     });
   } catch (err) {
     console.log(err);
     return next(
-      new AppError('There was an error sending an email, Try Again Later', 500)
+      new AppError("There was an error sending an email, Try Again Later", 500)
     );
   }
 });
@@ -102,7 +102,7 @@ exports.verifyEmail = catchAsync(async (req, res, next) => {
 exports.validateUser = catchAsync(async (req, res, next) => {
   const doc = await Transaction.find({ user: req.user.id });
   res.status(200).json({
-    status: 'success',
+    status: "success",
     data: {
       user: req.user,
       transactions: doc,
@@ -110,19 +110,18 @@ exports.validateUser = catchAsync(async (req, res, next) => {
   });
 });
 exports.login = catchAsync(async (req, res, next) => {
-  console.log('-------------------------------');
   const { email, password } = req.body;
   if (!email || !password) {
-    return next(new AppError('Please Provide Email and password', 400));
+    return next(new AppError("Please Provide Email and password", 400));
   }
   const user = await User.findOne({ email })
-    .select('+password')
-    .select('+emailVerified');
+    .select("+password")
+    .select("+emailVerified");
   //Comparing password
   if (!user || !(await user.correctPassword(password, user.password))) {
-    return next(new AppError('Incorrect Email or password', 401));
+    return next(new AppError("Incorrect Email or password", 401));
   } else if (!user || user.emailVerified === false) {
-    return next(new AppError('Email not verified yet', 401));
+    return next(new AppError("Email not verified yet", 401));
   }
   createLoginToken(user, 200, req, res);
 });
