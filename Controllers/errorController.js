1af39729fdaf480f1510020884bd0875982c753d
sami-lab <s.m.sami125@gmail.com>
2021-12-13 14:24:19 +0500

    error testing

--- Controllers/errorController.js
+++ Controllers/errorController.js
@@ -47,7 +47,7 @@ const sendErrProd = (err, res) => {
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
-  if (process.env.NODE_ENV !== "development") {
+  if (process.env.NODE_ENV == "development") {
     sendErrDev(err, res);
   } else {
     let error;
